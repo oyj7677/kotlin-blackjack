702c40f99e7cbc3cb9fdd69f3e1ffdbcb669949b
OYJ <oyj7677@gmail.com>
2023-12-11 20:39:58 +0900

    Refactor: 딜러 BUST 시 무조건 패배 조건 추가
    Docs: 기능목록 정리

--- src/main/kotlin/player/Dealer.kt
+++ src/main/kotlin/player/Dealer.kt
@@ -2,6 +2,8 @@ package player
 
 class Dealer : Participant() {
     fun isWinning(player: Player): Boolean {
+        if (status == Status.BUST) return false
+
         val dealerResultPoint = getResultPoint()
         val playerResultPoint = player.getResultPoint()
 
--- src/test/kotlin/blackJack/participant/DealerTest.kt
+++ src/test/kotlin/blackJack/participant/DealerTest.kt
@@ -12,19 +12,43 @@ class DealerTest {
 
     @Test
     fun `,다른 참여자의 카드를 받아 비교할 때, 승패가 반환된다`() {
-        // given :
+        // given : 딜러와 플레이어 카드 배포
+        // 딜러 21
         val dealer = Dealer()
         dealer.hit(PlayingCard(Suit.DIAMOND, CardRank.ACE))
         dealer.hit(PlayingCard(Suit.DIAMOND, CardRank.KING))
 
+        // 플레이어 4
         val player = Player("OYJ")
         player.hit(PlayingCard(Suit.DIAMOND, CardRank.ACE))
         player.hit(PlayingCard(Suit.DIAMOND, CardRank.TREE))
 
-        // when :
+        // when : 승패 비교
+        val actual: Boolean = dealer.isWinning(player)
+
+        // then : 딜러 승리 시 True
+        Assertions.assertThat(actual).isTrue()
+    }
+
+    @Test
+    fun `딜러의 상태가 BUST일 때, 딜러는 무조건 패배한다`() {
+        // given : 딜러와 플레이어 카드 배포
+        // 딜러 BUST
+        val dealer = Dealer()
+        dealer.hit(PlayingCard(Suit.DIAMOND, CardRank.FIVE))
+        dealer.hit(PlayingCard(Suit.DIAMOND, CardRank.KING))
+        dealer.hit(PlayingCard(Suit.DIAMOND, CardRank.KING))
+
+        // 플레이어 BUST
+        val player = Player("OYJ")
+        dealer.hit(PlayingCard(Suit.DIAMOND, CardRank.FIVE))
+        dealer.hit(PlayingCard(Suit.DIAMOND, CardRank.KING))
+        dealer.hit(PlayingCard(Suit.DIAMOND, CardRank.KING))
+
+        // when : 승패 비교
         val actual: Boolean = dealer.isWinning(player)
 
         // then :
-        Assertions.assertThat(actual).isTrue()
+        Assertions.assertThat(actual).isFalse()
     }
 }
